10f0301d4bbe92fcd6a0d2f8b2d3738dd23e0076
Viniciusslara <vinicius.lara2017@hotmail.com>
2023-11-02 21:03:25 -0300

    add: login page

---
+++ src/components/Header.tsx
@@ -0,0 +1,57 @@
+import icon from '../../public/logo.png';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import '../styles/header.css';
+
+
+
+export default function Header ({props}){
+
+    const navegar = useRouter();
+
+    const renderNavLinks = (props:string) => {
+        switch (props) {
+            case 'Home':
+                return (
+                    <>
+                        <span onClick={() => navegar.push('/register')}>Sair</span>
+                    </>
+                );
+
+            case 'Login':
+                return (
+                    <>
+                        <span onClick={() => navegar.push('/register')}><strong>Register</strong></span>
+                    </>
+                );
+
+            case 'Register':
+                return (
+                    <>
+                        <span onClick={() => navegar.push('/Login')}><strong>Entrar</strong></span>
+                    </>
+                );
+
+            default:
+                break;
+        }
+    }
+
+    return (
+        <div className="Header">
+            <div className='Header-logo'>
+                <Image
+                    src={icon}
+                    alt="Icone"
+                    height={200}
+                    width={200}
+                    priority />
+
+                <h1>iChat</h1>
+            </div>
+            <div className='Header-btns'>
+                {renderNavLinks(props)}
+            </div>
+        </div>
+    );
+}
