c87599b00b4e13d7ec07967c5582999f0a41d908
Viniciusslara <vinicius.lara2017@hotmail.com>
2023-11-08 22:48:31 -0300

    chore: login page title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,46 @@
 "use client";
 
 import Image from 'next/image'
+import Head from 'next/head';
 import Header from '@/components/Header'
 import '../styles/page.css';
 
 export default function Home() {
   return (
     <>
-        <Header props='Login'/>
-          <div className='message'>
-            <h1><strong>Connecting you with the world!</strong></h1>
-          </div>
-          <div className='login'>
-            <form action="">
-              <div className='Login-input'>
-                <label htmlFor="inputEmail">Email</label>
-                <input 
-                    type="email"
-                    name='email'      
-                    id='inputEmail'
-                    placeholder='Insert your email' 
-                />
-              </div>
-              <div className='Login-input'>
-                <label htmlFor="inputPassword">Password</label>
-                <input 
-                    type="password"
-                    name='password'      
-                    id='inputPassword'
-                    placeholder='Insert your password' 
-                />
-              </div>
+      <Head>
+        <title>Login</title>
+      </Head>
 
-              <button type='submit'>Enter</button>
-            </form>
-          </div>
+      <Header props='Login'/>
+      <div className='message'>
+        <h1><strong>Connecting you with the world!</strong></h1>
+      </div>
+        <div className='login'>
+          <form action="">
+            <div className='Login-input'>
+              <label htmlFor="inputEmail">Email</label>
+              <input 
+                type="email"
+                name='email'      
+                id='inputEmail'
+                placeholder='Insert your email' 
+              />
+            </div>
+          
+            <div className='Login-input'>
+              <label htmlFor="inputPassword">Password</label>
+              <input 
+                type="password"
+                name='password'      
+                id='inputPassword'
+                placeholder='Insert your password' 
+              />
+            </div>
+
+            <button type='submit'>Enter</button>
+          </form>
+        </div>
     
     </>
   )
