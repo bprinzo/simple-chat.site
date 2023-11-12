612413ce2ab8e226732c941c68ad106d07317061
Viniciusslara <vinicius.lara2017@hotmail.com>
2023-11-12 11:57:08 -0300

    add: home page attributes

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,11 +1,29 @@
 'use client';
 
 import Header from "@/components/Header";
+import ChatCard from "@/components/ChatCard";
+import '../../styles/home.css'
 
 export default function Home (){
     return(
         <>
             <Header props='Home'/>
+            <div className="id">
+                <h1><strong>Username</strong></h1>
+            </div>
+            <div className="container">
+                <div className="container_button">
+                    <button>Create Chat</button>
+                    <button>Enter Chat</button>
+                </div>
+                <h4><strong>Your Chats</strong></h4>
+                <div className="container_chatBox">
+                    <ChatCard/>
+                    <ChatCard/>
+                </div>
+
+
+            </div>
         </>
     )
 }
