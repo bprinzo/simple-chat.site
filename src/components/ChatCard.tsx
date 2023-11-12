e8035346db18d1e700b8997b724608e1de8605a2
Viniciusslara <vinicius.lara2017@hotmail.com>
2023-11-12 11:54:25 -0300

    chore: component to chat card in home page

---
+++ src/components/ChatCard.tsx
@@ -0,0 +1,18 @@
+
+import '../styles/chat_card.css'
+
+export default function ChatCard(){
+    return(
+        <>
+            <div className="chat_box">
+                <a href="/">
+                    <h3><strong>Grupo Sistemas Distribuidos</strong></h3>
+                    <p>#40028922</p>
+                </a>
+        
+            </div>
+
+        
+        </>
+    )
+}
